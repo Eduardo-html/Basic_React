3814e4a61ae176119ca5550bc03c8744d0f50ff3
Luiz Eduardo de Oliveira Silva <luize.silva@softtek.com>
2022-08-28 16:47:29 -0300

    github link

--- src/components/MeuNome.jsx
+++ src/components/MeuNome.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 const MeuNome = (props) =>{
     let nome = "Luiz";
     return (
@@ -6,6 +7,13 @@ const MeuNome = (props) =>{
             Meu nome é {nome}
             <br/>
             Seu nome é {props.x}
+        </h1>
+
+        <br/>
+        <br/>
+        <h1>
+          <p><strong><u>GITHUB</u></strong></p>
+          <a>https://github.com/Eduardo-html/Basic_React</a>
         </h1>
       </>
     );
