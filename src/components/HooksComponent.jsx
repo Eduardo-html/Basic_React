970b23d05bd2e1d4707a20029a8b6547a1b28dcb
Luiz Eduardo de Oliveira Silva <luize.silva@softtek.com>
2022-08-28 16:18:20 -0300

    ajuste hooks

--- src/components/HooksComponent.jsx
+++ src/components/HooksComponent.jsx
@@ -5,29 +5,17 @@ const Hooks = () => {
     let idade = 27;
     const [novaIdade, setNovaIdade] = useState(28);
 
-    const changeAge = () => {
-        return(
-            idade = 28
-        );
-    }
-
     const changeNewAge = () => {
         return (
             setNovaIdade(29)
         );
     }
 
-    {/*useEffect(() => {
-        console.log("Usou o Effect")
-    });*/}
-
     return (
         <div>
-            <p>Sua idade: {idade}</p>
-            <button onClick={changeAge}>Click para adicionar idade</button>
-            <p>Sua idade: {novaIdade}</p>
-            <button onClick={changeNewAge}>Click para alterar idade</button>
-            
+            <p>Sua idade anterior: {idade}</p>
+            <p>Sua idade atual: {novaIdade}</p>
+            <button onClick={changeNewAge}>Click para add +1 em idade</button>
         </div>
     );
 }
